244d90951673d881e05c9c16912f5614fc0a3ab1
zailua <gabrielmmc1@gmail.com>
2022-09-13 16:10:50 -0300

    alterando estilos

--- react-monitoria/src/App.jsx
+++ react-monitoria/src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <h1>Fala ai Jonathan</h1>
-    </div>
-  )
+	return (
+		<div className="App">
+			<Header />
+			<h1>Fala ai Jonathan</h1>
+		</div>
+	)
 }
 export default App
