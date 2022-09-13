8052e3d3ec5d7bc6b5e1cf709f782acdfec2129c
Gabriel Nascimento <gabrielnascimento_17@hotmail.com>
2022-09-13 15:40:39 -0300

    desmembrei parte da estilização do component nav para dentro do arquivo css do componente

--- react-monitoria/src/components/Nav/index.jsx
+++ react-monitoria/src/components/Nav/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './style.css'
 
 function Nav() {
   return (
     <nav>
-      <ul class="menu">
+      <ul className='menu'>
         <li><a href="./home.html">Home</a></li>
         <li><a href="./destinos.html">Destinos</a></li>
         <li><a href="./promocao.html">Promoções</a></li>
