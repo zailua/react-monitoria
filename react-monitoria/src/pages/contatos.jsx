a5d7ea5858818ac98defcf7fb2faedaef06b1a80
gabrielzom <gabrielnascimento_17@hotmail.com>
2022-09-20 13:59:15 -0300

    added a examples of variables, arrays and objects

--- react-monitoria/src/pages/contatos.jsx
+++ react-monitoria/src/pages/contatos.jsx
@@ -3,6 +3,50 @@ import Header from '../components/Header'
 import './contatos.css'
 
 export default function contatos() {
+
+	const cpf = '463.312.678-99';
+	const telefone = '+55 (11) 9 7641-2211';
+	const array = [ 1, 2, 3, 4, 5, 6 ];
+	const cliente = {
+		nome: 'José',
+		sobrenome: 'Silva',
+		nascimento: new Date().toISOString(),
+		idade: 32
+	}
+	const clientes = [
+		{
+			nome: 'Maria',
+			sobrenome: 'José',
+			nascimento: new Date().toISOString(),
+			idade: 52
+		},
+		{
+			nome: 'Marcio',
+			sobrenome: 'Santos',
+			nascimento: new Date().toISOString(),
+			idade: 43
+		},
+		{
+			nome: 'Ana',
+			sobrenome: 'Maria',
+			nascimento: new Date().toISOString(),
+			idade: 26
+		},
+		{
+			nome: 'Laura',
+			sobrenome: 'Gomes',
+			nascimento: new Date().toISOString(),
+			idade: 19
+		},
+		{
+			nome: 'João',
+			sobrenome: 'Barbosa',
+			nascimento: new Date().toISOString(),
+			idade: 28
+		},
+	]
+
+
 	return (
 		<div>
 			<Header />
@@ -17,6 +61,20 @@ export default function contatos() {
 							<div class="screen-body-item left">
 								<div class="app-title">
 									<span>Entre em Contato</span>
+									{/* <br/>
+									<h5>Demo map</h5>
+									<ul>
+								 		{array.map(e => 
+											<li>Recode Pro - Turma {e}</li>
+										)}
+									</ul>
+									<br/>
+									<h5>Demo filter</h5>
+									<ul>
+								 		{array.filter(e => e > 3).map(e => 
+											<li>Recode Pro - Turma {e}</li>
+										)}
+									</ul> */}
 								</div>
 							</div>
 							<div class="screen-body-item">
