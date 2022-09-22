694f7c82b80dbc1ce6ab48ed11a76258618c8e10
Gabriel Nascimento <gabrielnascimento_17@hotmail.com>
2022-09-22 14:53:59 -0300

    exemplos do filter

--- react-monitoria/src/pages/contatos.jsx
+++ react-monitoria/src/pages/contatos.jsx
@@ -5,7 +5,33 @@ import './contatos.css'
 export default function contatos() {
 
 	const cpf = '463.312.678-99';
+	// '46331267899'
+
+	const isNumber = (value) => !isNaN(value)
+
+	console.log('ANTES DO SPLIT --> ', cpf)
+
+	const cpfVetorSplit = cpf.split('')
+	console.log('ANTES DO FILTER --> ', cpfVetorSplit)
+
+	const cpfVetorSomenteNumeros = cpfVetorSplit.filter((caractere) => isNumber(caractere))
+	console.log('DEPOIS DO FILTER --> ', cpfVetorSomenteNumeros)
+
+	let cpfSomenteNumeros = '';
+
+	cpfVetorSomenteNumeros.forEach(numero => {
+		cpfSomenteNumeros += numero;
+	})
+
+	console.log('DEPOIS DO FOREACH --> ', cpfSomenteNumeros)
+
 	const telefone = '+55 (11) 9 7641-2211';
+	// '+5511976412211'
+
+	const nascimento = '19/03/2002'
+	// '2002-03-19 00:00:00'
+	// '2002-03-19'
+
 	const array = [ 10.99, 99.90, 105.5, 4.99, 5.20, 6.0 ];
 	const cliente = {
 		nome: 'José',
@@ -52,12 +78,12 @@ export default function contatos() {
 
 	let index = 0;
 	while (index < array.length) {
-		console.log('USANDO WHILE -> ', array[index])
+		//console.log('USANDO WHILE -> ', array[index])
 		index++
 	}
 
 	for (let i = 0; i < array.length; i++) {
-		console.log('USANDO FOR -> ', array[i])
+		//console.log('USANDO FOR -> ', array[i])
 	}
 
 	function funcaoNormal(elemento) {
@@ -74,8 +100,10 @@ export default function contatos() {
 	
 	//const array = [ 10.99, 99.90, 105.5, 4.99, 5.20, 6.0 ];
 	const arrayComDesconto = array.map((preco) => preco - (preco * 0.15))
+	//console.log(arrayComDesconto)
+	//retorno esperado: [ 9.3415, 84.915, 89.675, 4.2415, 4.42, 5.1 ]
 
-	console.log(arrayComDesconto)
+
 
 	return (
 		<div>
